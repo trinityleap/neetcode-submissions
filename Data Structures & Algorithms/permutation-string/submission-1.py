05c7885036c507af4417f5900e496296acb0bdd4
trinityleap <157149924+trinityleap@users.noreply.github.com>
2026-07-27 10:59:30 -0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        """
+        sliding window
+
+        bases:
+            is s2 at least as long as s1 
+                or counter s2 > s1 
+                otherwise containing perm is not possible
+        
+        window size of s1, 
+            slide until counter window = counter s1
+        - how to track whats in window?
+        - computing counter at each update might be inefficient
+
+        """
+        perm = Counter(s1)
+
+        # base
+        if perm > Counter(s2):
+            return False
+        
+        #
+        left, right = 0, len(s1) - 1
+        while right < len(s2):
+            curr = Counter(s2[left:right+1])
+            
+            if perm == curr:
+                return True
+
+            left += 1
+            right += 1
+
+        return False
